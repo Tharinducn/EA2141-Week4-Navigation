50d984791e8deec5f9e57b07cb8ed23df66389de
Tharindu <tharindunisal@gmail.com>
2024-02-10 22:56:29 +0530

    Add comments

--- app/_layout.js
+++ app/_layout.js
@@ -2,10 +2,12 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
+    // Stack is used to define the navigation stack
     <Stack>
+        {/* Screen is used to define the screen on the stack */}
         <Stack.Screen name="index" options={{
-            headerTitle: "Login",
-            headerShown: false
+            headerTitle: "Login", // Set the title of the screen
+            headerShown: false // Hide the header
         }}/>
         <Stack.Screen name="home" options={{
             headerTitle: "Home",
