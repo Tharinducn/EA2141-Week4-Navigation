82e31bdbf83b8de3386c9d147b1f570415ca089e
Tharindu <tharindunisal@gmail.com>
2024-02-10 20:54:03 +0530

    Create stack navigation

---
+++ app/home.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>This is Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green'
+    },
+});
---
+++ app/index.js
@@ -0,0 +1,19 @@
+import { View, StyleSheet} from "react-native";
+import { Link } from "expo-router";
+
+export default function Page() {
+    return (
+        <View style={styles.container}>
+            <Link href={'/home'}>Go to Home</Link>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'orange'
+    },
+});
