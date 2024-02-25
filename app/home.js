5bf65cf9cd65450f6fd1418afd59a4aea60d0fec
Tharindu <tharindunisal@gmail.com>
2024-02-25 10:03:45 +0530

    Add id to data

--- app/home.js
+++ app/home.js
@@ -1,32 +1,39 @@
 import { FlatList, StyleSheet, Text, View, Image} from "react-native";
 
+// Data for the FlatList
 const data = [
   {
+    id: 1,
     title: 'Margarita',
     description: 'Rub the rim of the glass with the lime slice to make the salt stick to it. Shake the other ingredients with ice, then carefully pour into the glass.',
     image: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg'
   },
   {
+    id: 2,
     title: 'Blue Margarita',
     description: 'Rub rim of cocktail glass with lime juice. Dip rim in coarse salt. Shake tequila, blue curacao, and lime juice with ice, strain into the salt-rimmed glass, and serve.',
     image: 'https://www.thecocktaildb.com/images/media/drink/bry4qh1582751040.jpg'
   },
   {
+    id: 3,
     title: "Tommy's Margarita",
     description: 'Shake and strain into a chilled cocktail glass.',
     image: 'https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg'
   },
   {
+    id: 4,
     title: 'Whitecap Margarita',
     description: 'Place all ingredients in a blender and blend until smooth. This makes one drink.',
     image: 'https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg'
   },
   {
+    id: 5,
     title: 'Strawberry Margarita',
     description: 'Rub rim of cocktail glass with lemon juice and dip rim in salt. Shake schnapps, tequila, triple sec, lemon juice, and strawberries with ice, strain into the salt-rimmed glass, and serve.',
     image: 'https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg'
   },
   {
+    id: 6,
     title: 'Smashed Watermelon Margarita',
     description: 'In a mason jar muddle the watermelon and 5 mint leaves together into a puree and strain. Next add the grapefruit juice, juice of half a lime and the tequila as well as some ice. Put a lid on the jar and shake. Pour into a glass and add more ice. Garnish with fresh mint and a small slice of watermelon.',
     image: 'https://www.thecocktaildb.com/images/media/drink/dztcv51598717861.jpg'
@@ -34,6 +41,8 @@ const data = [
 ];
 
 export default function Home() {
+
+  // Item to render for the FlatList
   const renderItem = ({ item }) => (
     <View style={styles.itemContainer}>
       <View style={styles.textContainer}>
@@ -44,6 +53,7 @@ export default function Home() {
     </View>
   );
 
+  // Separator for each data item
   const renderSeparator = () => (
     <View style={styles.separator} />
   );
@@ -52,7 +62,7 @@ export default function Home() {
     <FlatList
       data={data}
       renderItem={renderItem}
-      keyExtractor={(item, index) => index.toString()}
+      keyExtractor={(item) => item.id} // Unique key for each item
       ItemSeparatorComponent={renderSeparator}
     />
   )
